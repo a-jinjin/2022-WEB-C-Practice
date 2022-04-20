11970b06204ba305968f97c85a48fca94b5b3550
a-jinjin <99878175+a-jinjin@users.noreply.github.com>
2022-04-20 17:34:26 +0900

    자바스크립트 써보기 : console.dir와 console.log를 사용하는 버릇 들이기

---
+++ [220420] javaScriptTest/javascriptTest.js
@@ -0,0 +1,81 @@
+// let one = "1";
+// 변수를 선언할것이다. 이름은 one이다. 선언한 김에 값도 대입해주겠다. 값은 숫자타입의 1이다.
+
+// let two = 2;
+
+// console.log(one);
+// ==> js야, 변수 one이 뭔지 모르겠는데, 실행창에 기록좀 남겨줘
+
+// console.log(typeof one);
+// ==> js야, 변수 one 데이터타입이 뭔지 모르겠다. 뭐냐?
+
+
+
+
+// let one = "1";
+// let two = 1;
+
+// console.log(typeof (one+two));
+
+
+
+
+// let lifeis = "삶은";
+// let boiled = "계란";
+
+// console.log(typeof(lifeis + boiled));
+// js야 lifeis라는 변수와 boiled라는 변수를 더한 다음에 데이터타입 체크좀 해달라.
+
+
+
+
+// let na = "공욱재는 미남이다.";
+
+// if(typeof na === "string") {
+// // ?? if === js야 만약에, 소괄호에 있는 내용이 맞으면 중괄호에 있는 것좀 실행시켜달라.
+
+//   console.log("hahahahahahahahaha");
+// }
+
+
+
+// let coffee = {
+//   name: "americano",
+//   store: "moonbucks"
+// }
+// console.log(typeof coffee);
+// // 참 영역 
+// if(coffee.name === "espresso") {
+//   console.log("wake up");
+// }
+// // 거짓 영역
+// else {
+//     console.log("deep sleep");
+// }
+
+
+
+// let a = 1;
+// let b = 2;
+// let result = "값이 다릅니다.";
+
+// if(a === b) {
+//   console.log("값이 같습니다.");
+// }
+// else{
+//   console.log(result);
+//   // 거짓인 경우, 변수인 result를 console에 보여달라. ==> console에 '기록'할 뿐. 변수의 값이 무엇인지는 관심이 없다.
+// }
+
+
+
+let a = 1;
+let b = 2;
+
+if(a !== b) {
+  // !==  ---> "같지 않으면"
+  console.log('값이 같습니다.');
+}
+else{
+  console.log('값이 다릅니다.');
+}
---
+++ [220420] javaScriptTest/test.js
@@ -0,0 +1,25 @@
+let apple = "사과";
+
+console.log(typeof apple);
+// apple의 데이터타입을 보여달라
+
+// if (typeof apple === "string") {
+//   console.log("냠냠");
+// }
+// else {
+//   console.log("틀렸다네");
+// }
+
+// if (typeof apple === "number") {
+//   console.log("냠냠");
+// }
+// else {
+//   console.log("틀렸다네");
+// }
+
+if (typeof apple !== "number") {
+  console.log("냠냠");
+}
+else {
+  console.log("틀렸다네");
+}
